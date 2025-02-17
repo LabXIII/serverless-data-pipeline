0610270ebbb8b10b06571e8712a3f97dfd72a41f
LabXIII <mindwirelabs@gmail.com>
2025-02-17 03:32:32 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,25 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  default     = "serverless-data-pipeline"
+}
+
+variable "api_url" {
+  description = "API URL for data source"
+  default     = "https://api.example.com/data"
+}
+
+variable "lambda_code_bucket" {
+  description = "S3 bucket for Lambda code"
+  default     = "lambda-code-bucket"
+}
+
+variable "lambda_code_key" {
+  description = "S3 key for Lambda code zip"
+  default     = "function.zip"
+}
 
